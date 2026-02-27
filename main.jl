dbd3f20b540529e24717bf605e26e8758ce00046
Bruno Ardenghi <bruno.samuel@ufrgs.br>
2026-02-27 18:13:05 -0300

    fix rng & refactor

--- main.jl
+++ main.jl
@@ -2,62 +2,68 @@ using Lux, MLUtils, Optimisers, Zygote, OneHotArrays, Random, Printf
 using MLDatasets: MNIST
 using SimpleChains: SimpleChains
 
-function load_MNIST(; batch_size)
-    train_data = MNIST(:train)
-    test_data = MNIST(:test)
+function load_MNIST(rng; batchsize::Int)
+    preprocess(data) = (
+        reshape(data.features, 28, 28, 1, :),
+        onehotbatch(data.targets, 0:9),
+    )
 
-    x_train = Float32.(reshape(train_data.features, 28, 28, 1, :))
-    y_train = onehotbatch(train_data.targets, 0:9)
-
-    x_test = Float32.(reshape(test_data.features, 28, 28, 1, :))
-    y_test = onehotbatch(test_data.targets, 0:9)
+    X_train, y_train = preprocess(MNIST(:train))
+    X_test, y_test = preprocess(MNIST(:test))
 
     return (
-        DataLoader(
-            (x_train, y_train);
-            batchsize = batch_size, shuffle = true
-        ),
-        DataLoader(
-            (x_test, y_test);
-            batchsize = batch_size, shuffle = false
-        ),
+        DataLoader((X_train, y_train); batchsize, shuffle = true, rng),
+        DataLoader((X_test, y_test); batchsize, shuffle = false),
     )
 end
 
 function accuracy(model, θ, s, dataloader)
-    s = Lux.testmode(s)
-    correct = sum(dataloader) do (x, y)
-        sum(onecold(y) .== onecold(Array(first(model(x, θ, s)))))
+    s_test = Lux.testmode(s)
+    correct, total = 0, 0
+
+    for (X, y) in dataloader
+        pred = Array(first(model(X, θ, s_test)))
+        correct += sum(onecold(pred) .== onecold(y))
+        total += size(y, 2)
     end
-    total = sum(size(y, 2) for (x, y) in dataloader)
+
     return (correct / total) * 100
 end
 
-function train(model; rng = Random.default_rng())
-    train_dataloader, test_dataloader = load_MNIST(batch_size = 128)
+function train(
+        model;
+        rng = Random.default_rng(),
+        batchsize = 128,
+        n_epochs = 10,
+        optimizer = Adam(3.0f-4),
+        lossfn = CrossEntropyLoss(; logits = Val(true)),
+        ad_backend = AutoZygote(),
+    )
+    train_dataloader, test_dataloader = load_MNIST(rng; batchsize)
     θ, s = Lux.setup(rng, model)
-    ts = Training.TrainState(model, θ, s, Adam(3.0f-4))
-    lossfn = CrossEntropyLoss(; logits = Val(true))
-    vjp = AutoZygote()
-
-    n_epochs = 50
+    ts = Training.TrainState(model, θ, s, optimizer)
 
     for epoch in 1:n_epochs
-        stime = time()
-        for (x, y) in train_dataloader
-            _, _, _, ts = Training.single_train_step!(vjp, lossfn, (x, y), ts)
+        Δt = @elapsed begin
+            for (X, y) in train_dataloader
+                grads, loss, stats, ts = Training.single_train_step!(
+                    ad_backend, lossfn, (X, y), ts
+                )
+            end
         end
-        ttime = time() - stime
 
-        if epoch == 1 || epoch % 10 == 0 || epoch == n_epochs
-            train_acc = accuracy(model, ts.parameters, ts.states, train_dataloader)
-            test_acc = accuracy(model, ts.parameters, ts.states, test_dataloader)
-            @printf "[%2d/%2d] \t Time %.2fs \t Training Accuracy: %.2f%% \t Test Accuracy: %.2f%%\n" epoch n_epochs ttime train_acc test_acc
+        if epoch == 1 || epoch % 5 == 0 || epoch == n_epochs
+            train_acc = accuracy(
+                model, ts.parameters, ts.states, train_dataloader
+            )
+            test_acc = accuracy(
+                model, ts.parameters, ts.states, test_dataloader
+            )
+            @printf "[%2d/%2d] \t Time %.2fs \t Training Accuracy: %.2f%% \t Test Accuracy: %.2f%%\n" epoch n_epochs Δt train_acc test_acc
         end
     end
 
     return ts
-
 end
 
 model = Chain(
@@ -71,4 +77,4 @@ model = Chain(
 )
 
 simple_chains_model = ToSimpleChainsAdaptor((28, 28, 1))(model)
-trained_state = train(simple_chains_model)
+trained_state = train(simple_chains_model; rng = Xoshiro(42))
