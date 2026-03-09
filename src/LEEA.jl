3845671fd695e691187ab711de9b24fc008c6893
Bruno Ardenghi <bruno.samuel@ufrgs.br>
2026-03-09 19:29:50 -0300

    add assexual reproduction

--- src/LEEA.jl
+++ src/LEEA.jl
@@ -1,8 +1,10 @@
 module LEEA
 
 import Lux
-using Random: Xoshiro
+using Random: Xoshiro, rand!
 using Polyester: @batch
+using StatsBase: Weights, sample
+using Base.Threads: nthreads, threadid
 using ..Data: load_MNIST
 using ..Models: CNN_2C2D_MNIST
 
@@ -11,37 +13,69 @@ export train_LEEA
 const logitcrossentropy = Lux.CrossEntropyLoss(; logits = Val(true))
 
 @kwdef struct LEEAConfig
-    P::Int = 1000       # population size
+    N::Int = 1000       # population size
     r::Float32 = 0.04   # mutation rate
-    m₀::Float32 = 0.03  # initial mutation power
-    γ::Float32 = 0.99   # mutation power decay
+    m::Float32 = 0.03   # mutation power
+    γₘ::Float32 = 0.99  # mutation power decay
     pₛ::Float32 = 0.4   # selection proportion
     s::Float32 = 0.5    # sexual reproduction proportion
-    d::Float32 = 0.2    # fitness inheritance decay
+    γᵢ::Float32 = 0.2   # fitness inheritance decay
+end
+
+function mutate!(O, P, as_idx, U₁, U₂, rngs, r, m, γₘ)
+    return @batch for i in 1:length(as_idx)
+        tid = threadid()
+        u₁ = @view U₁[:, tid]
+        u₂ = @view U₂[:, tid]
+
+        rand!(rngs[tid], u₁)
+        rand!(rngs[tid], u₂)
+
+        p_idx = as_idx[i]
+
+        @. O[:, i] = P[:, p_idx] + (u₁ < r) * m * (2.0f0 * u₂ - 1.0f0)
+    end
 end
 
 function train_LEEA(; seed::Int, batchsize::Int, generations::Int)
+    n_threads = nthreads()
     rng = Xoshiro(seed)
-    (; P, r, m₀, γ, pₛ, s, d) = LEEAConfig()
+    rngs = [Xoshiro(seed + t) for t in 1:n_threads]
+    (; N, r, m, γₘ, pₛ, s, γᵢ) = LEEAConfig()
 
     train_dataloader, _ = load_MNIST(; rng, batchsize, balanced = true)
     model = CNN_2C2D_MNIST
     _, st = Lux.setup(rng, model)
+    st = Lux.testmode(st)
+    θ_len = Lux.parameterlength(model)
 
-    pop = stack(Vector(Lux.initialparameters(rng, model).params) for _ in 1:P)
-    f = zeros(Float32, P)
+    P = stack(Vector(Lux.initialparameters(rng, model).params) for _ in 1:N)
+    O = similar(P)
+
+    f = Vector{Float32}(undef, N)
+
+    num_as = round(Int, (1 - s) * N)
+    U₁ = Matrix{Float32}(undef, θ_len, num_as)
+    U₂ = Matrix{Float32}(undef, θ_len, num_as)
 
     for i in 1:generations
         X, Y = popfirst!(train_dataloader)
 
-        @batch for j in 1:P
-            θⱼ = NamedTuple{(:params,)}((@view(pop[:, j]),))
+        @batch for j in 1:N
+            θⱼ = NamedTuple{(:params,)}((@view(P[:, j]),))
             Ŷ, _ = model(X, θⱼ, st)
-            f[j] = 1 / (1 + logitcrossentropy(Ŷ, Y))
+            f[j] = 1.0f0 / (1.0f0 + logitcrossentropy(Ŷ, Y))
         end
 
-        wheel = partialsort(f, 1:round(Int, pₛ * P), rev = true)
-        @show wheel
+        wheel_idx = partialsortperm(f, 1:round(Int, pₛ * N), rev = true)
+        wheel_w = Weights(@view f[wheel_idx])
+
+        as_idx = sample(rng, wheel_idx, wheel_w, num_as)
+
+        mutate!(O, P, as_idx, U₁, U₂, rngs, r, m, γₘ)
+        m *= γₘ
+
+        @show O[1:10, 1]
     end
 
     return
